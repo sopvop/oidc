503dbc41b2bad937c30c7662291b506743a41ac9
Leonid Onokhov <sopvop@gmail.com>
2018-04-03 11:46:39 +0000

    WTF

--- shell.nix
+++ shell.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import ./../smassetman-server/pkgs.nix {} }:
+let
+  overrides = import ./../stagex-store/packages.nix;
+  base = import (import ./../stagex-store/pkgs.nix) { overlays = [overrides];};
+in
+{ pkgs ? base }:
 let
   p = pkgs.haskellPackages.callCabal2nix "oidc" ./. {};
 
